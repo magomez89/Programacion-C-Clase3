69db16b4823f14a1c991ca04fa43763659058816
magomez89 <manuel.pc23@gmail.com>
2015-03-19 19:08:12 +0100

    Subiendo ejercicios 3
    Signed-off-by: magomez89 <manuel.pc23@gmail.com>

---
+++ ejercicios/3/main.c
@@ -0,0 +1,36 @@
+#include "concesionario.h"
+
+int main(void)
+{
+	struct coche *c1, *c2;
+	struct concesionario *con;
+	char info[4000];
+	c1 = curso_coche_alloc();
+
+	curso_coche_attr_set_str(c1, CURSO_COCHE_ATTR_MATRICULA, "ES1244D");
+	curso_coche_attr_set_str(c1, CURSO_COCHE_ATTR_MARCA, "minicoche");
+	curso_coche_attr_set_u32(c1, CURSO_COCHE_ATTR_ID, 0);
+
+	c2 = curso_coche_alloc();
+
+	curso_coche_attr_set_str(c2, CURSO_COCHE_ATTR_MATRICULA, "ES1233E");
+	curso_coche_attr_set_str(c2, CURSO_COCHE_ATTR_MARCA, "fantastico");
+	curso_coche_attr_set_u32(c2, CURSO_COCHE_ATTR_ID, 0);
+
+	con = curso_concesionario_alloc();
+
+	curso_concesionario_attr_set_str(con, CURSO_CONCESIONARIO_ATTR_DUENO,
+					 "Alvaro");
+
+	curso_concesionario_attr_set_coche(con, CURSO_CONCESIONARIO_ATTR_COCHE,
+					   c1);
+
+	curso_concesionario_attr_set_coche(con, CURSO_CONCESIONARIO_ATTR_COCHE,
+					   c2);
+
+	curso_concesionario_snprintf(info, sizeof(info), con);
+	printf("%s", info);
+
+	curso_concesionario_free(con);
+	return 0;
+}
